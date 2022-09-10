737f25500764c73b90982433a91ff0fac91eca1f
Phong Tran <tranthanhphongg17@gmail.com>
2022-09-10 20:24:52 +0700

    code index and category

--- controllers/colorController.js
+++ controllers/colorController.js
@@ -1,13 +1,13 @@
 let controller = {};
 let models = require('../models');
-let Category = models.Category;
+let Color = models.Color;
 
 controller.getAll = () => {
     return new Promise((resolve, reject) => {
-        Category
+        Color
             .findAll({
-                attributes: ['id', 'name', 'imagepath', 'summary'],
-                include: [{model: models.Product}]
+                attributes: ['id', 'name', 'imagepath', 'code'],
+                include: [{model: models.ProductColor}]
             })
             .then(data => resolve(data))
             .catch(error => reject(new Error(error)));
